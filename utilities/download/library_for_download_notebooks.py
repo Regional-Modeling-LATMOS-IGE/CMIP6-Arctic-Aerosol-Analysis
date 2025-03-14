3f6c9af5a49a693f549248be734fc121811b8f46
gibonil <188331343+gibonil@users.noreply.github.com>
2025-03-14 15:39:34 +0100

    commented the created_dir function and removed the now useless moving function

--- utilities/download/library_for_download_notebooks.py
+++ utilities/download/library_for_download_notebooks.py
@@ -2,7 +2,6 @@
 
 """
 This small library is useful for the notebooks found in the download folder.
-It holds several functions that call for bash procedures and [...]
 
 Author : GIBONI Lucas
 
@@ -14,15 +13,15 @@ Feel free to copy, adapt and modify it under the provided license on github.
 ##################################
 
 import os  # to handle path's management
-import shutil
-import subprocess  # to run the wget code from python
+import shutil # to handle the removal of a folder tree
+
 
 #############################################
 ### DEFINITION OF THE SPECIFIC EXCEPTIONS ###
 #############################################
 
-class InputPathError(Exception):
-    ''' Raise when the parent path ends with a '/' or the child path starts with a '/' '''
+class FileExistsError(Exception):
+    ''' Raise when the path already exists and isn't a regular file or folder '''
 
 ###################################
 ### DEFINITION OF THE FUNCTIONS ###
@@ -30,109 +29,59 @@ class InputPathError(Exception):
 
 
 # ================ CREATE_DIR ================ #
-def create_dir(parent_path: str, name: str, clear: bool = False) -> str:
-    """Create the entire chain of folders and optionally empty the contents of the last one or delete the file with this name."""
-    
-    path = os.path.join(parent_path, name)
-    if clear and os.path.lexists(path):
-        if os.path.isfile(path) or os.path.islink(path):
-            os.remove(path)
-            
-        else if os.path.isdir(path):
-            shutil.rmtree(path)
 
-        else
-            raise FileExistsError("Path already exists and isn't a regular file or folder")
-
-    os.makedirs(path, exist_ok = True)
-
-    return path
-
-# ================ MV_DOWNLOADED_DATA ================ #
-
-def mv_downloaded_data(current_dir : str ,future_dir : str) :
-
+def create_dir(parent_path: str, name: str, clear: bool = True) -> str:
     """
     ### DEFINITION
 
-    This function moves the downloading folder in cuto the future_dir folder
-    WARNING : This wont work if you put your '/' wildly. At the end of a given path there should be not '/'.
+    Create the entire chain of folders and optionally empty the contents of the last one or delete the file with this name.
     
     ### INPUTS 
 
-    PARENT_DIR : STR | parent directory of the to be created directory : it must exist or an error will be sent
+    PARENT_PATH : STR | path of the parent directory of the to be created structure
     
-    TO_BE_CREATED_DIR : STR | the directory that is to be created : it can be a folder or a path. 
-    if it's a path, several folders and subfolders will be created.
+    NAME : STR | the structure that is to be created : it can be a folder, a path or a symbolic link.
+    If it is a path, the whole tree will be created.
+
+    CLEAR : STR | boolean to define if we clear the structure if it already exists.
+    default : True
 
     ### OUTPUT
 
-    FULL_PATH : STR | the full path of the folders and maybe subfolders created.
+    PATH : STR | the full path of the folders and maybe subfolders created.
     """
-
-    ### WE DEFINE THE FULL PATH ###
-
-    ## We try to define the full_path variable ##
     
-    # We check that at the end of parent_dir there is no '/' #
+    ### CREATE THE FULL PATH ###
+    
+    path = os.path.join(parent_path, name)
 
-    try :
+    ### CLEARING PROCEDURE IF THE PATH/SYMLINK ALREADY EXISTS ###
 
-        # If the end of parent_dir there is '/' we raise an error #
+    ## Test if we need to clear and if the path/symlink already exists ##
+    
+    if clear and os.path.lexists(path): 
+
+        ## If it is a path or a symlink we remove it ##
         
-        if parent_dir[-1] == '/' :
-
-            raise InputPathError
-
-        elif to_be_created_dir[0] == '/' :
-
-            raise InputPathError
-
-        else :
-
-            full_path = parent_dir + "/" + to_be_created_dir
+        if os.path.isfile(path) or os.path.islink(path):
             
-    except InputPathError as error :
+            os.remove(path)
 
-        raise InputPathError("neither parent_dir variable should end with a '/' nor to_be_created_dir variable start with a '/'")
+        ## If it is a path we remove all the tree ##
         
-    ### WE TRY TO MAKE THE FOLDER ###
-    
-    ## We try to make the folder but it will raise an error if parent_dir is wrong ##
-    
-    try : 
-    
-        ## We first check if to_be_created_dir already exists or not ##
-    
-        # If it exists : we remove it #
-        
-        if os.path.isdir(full_path) :
-    
-            # We run the rm -rf command from python to erase the folder and its content #
-    
-            subprocess.run(
-            "rm -rf {}".format(full_path), shell=True, check=True
-        )
-            print("Pre-existing folder at {} removed".format(full_path))
-        
-        ## We run the mkdir command from python to make the folder ##
-            
-        subprocess.run(
-            "mkdir  -p {}".format(full_path), shell=True, check=True  # -p option allows for simultaneous folder and subfolder creation
-        )
-    
-        print("New folder at {} created".format(full_path))
-    
-    ## An error was caught : storage_dir does not exist ##
-    
-    except FileNotFoundError as error:
-    
-        print("The storage_dir variable is wrong, such a path does not exists" + " : " + parent_dir + "\n")
-    
-        raise 
+        else if os.path.isdir(path):
+            shutil.rmtree(path)
 
-    return full_path
+        ## If we do not know what this is ##
+        
+        else
+            raise FileExistsError("Path already exists and isn't a regular file or folder")
 
+    ### MAKE THE DIRECTORY ###
+
+    os.makedirs(path, exist_ok = True) # if it exists : no error is sent
+
+    return path
 
 if __name__ == '__main__':
     pass
