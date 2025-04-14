fc89b285c352706e976ab07dd559be7b4ab2ef99
gibonil <188331343+gibonil@users.noreply.github.com>
2025-04-14 09:30:54 +0200

    coded function to extract unique dict keys and to add one var to a dataset -> need to keep going with the routine for the climatology cmip6 | also ststarted to clean analysis notebook accordingly

--- utilities/prepare_data/extract_climatologies_datasets.py
+++ utilities/prepare_data/extract_climatologies_datasets.py
@@ -88,32 +88,131 @@ variable_id = [
     "rlus",
 ]
 
+#######################################################
+### GENERATE DICTIONNARY KEYS WITHOUT THE VARIABLES ###
+#######################################################
+
 
 def generate_per_model_dict_key(dict_cmip6 : dict):
-    
+
+    """
+
+    ### DEFINITION ###
+
+    This function receives the dictionnary keys from the raw loaded data dictionnary and change the variables with a *.
+    It thens proceeds to make a unique key per model.variant couple and save them into a numpy array.
+
+    ### INPUTS ###
+
+    DICT_CMIP6 : DICT | dictionnary holding the raw loaded data
+
+    ### OUTPUTS ###
+
+    KEY_WITHOUT_VARIABLE_UNIQUE : NUMPY ARRAY OF STR | array with the one key per model with a * where the variables are written
+
+    """
+
+    ### RETRIEVE THE RAW LOADED DATA KEYS ###
+
+    ## List of the keys ##
+
     list_keys = list(dict_cmip6.keys())
+
+    ## Number of keys ##
 
     n_keys = len(list_keys)
 
-    keys_without_variable = np.empty(n_keys, dtype=object)  # otherwise it truncates the str
+    ## Generate the output array with the known number of keys ###
 
+    keys_without_variable = np.empty(n_keys, dtype=object)  # dtype = object otherwise it truncates the str
+
+    ### LOOP OVER THE DICTIONNARY KEYS ###
 
     for ii, key in enumerate(list_keys):
 
+        ## Split the keys where there is a "." ##
+
         splitted_key = key.split(".")
 
-        # copy the splitted key
+        ## Copy the splitted key ##
 
         splitted_key_without_var = splitted_key
 
-        # remove the variable
+        ## Remove the variable and replace it with a * ##
 
         splitted_key_without_var[-2] = "*"
 
+        ## Save it into the output numpy array ##
+
         keys_without_variable[ii] = splitted_key_without_var
 
-    # remove the duplicates (not the more efficient way of doing it but simpler)
+    ### REMOVE THE DUPLICATES ###
 
     keys_without_variable_unique = np.unique(keys_without_variable)
 
-    return
+    return keys_without_variable_unique
+
+############################################
+### ADD ONE VARIABLE TO A XARRAY DATASET ###
+############################################
+
+def add_one_variable_to_dataset(variable_name: str, var_datarray, modify_data:bool=False, dataset:bool=None):
+
+    """
+    ### DEFINITION
+
+    This function adds the variable_name variable to a xarray dataset with the var_datarray data array. 
+    If no datasets are provided, it will initialize them provided that modify_data is set to True.
+
+    ### INPUTS
+
+    VARIABLE_NAME : STR | variable to be added to the dataset
+
+    VAR_DATARRAY : XARRAY DATA ARRAY | data array holding the variable variable_name to add to the xarray dataset
+
+    MODIFY_DATA : BOOL | variable to say if we want to modify the input dataset
+
+    DATASET : XARRAY DATASET | dataset without the new variable
+
+    ### OUPUTS
+
+    DATASET : XARRAY DATASET | dataset with the new variable 
+
+    """
+
+    ### MAKE CLIMATOLOGIES ###
+
+    ## Produce the climatology ##
+
+    var_climatology = var_datarray.temporal.climatology(
+        variable_name, "month", weighted=True
+    )  # we generate a monthly climatology
+
+
+    ### ADDING THE VARIABLE TO THE DATASET ###
+
+    ## Check wether we need to modify the dataset or not : if false initializes it / if true fills the provided ones ##
+
+    if not modify_data:
+
+        dataset = var_climatology
+
+    else:
+
+        dataset[variable_name] = (
+            ("time", "lat", "lon"),
+            var_climatology[variable_name].values,
+        )
+
+    ### DIFFERENT CORRECTIONS ###
+
+    ## Test that the cloud fractions are expressed as fraction and modify it ##
+
+    if variable_name == "clt":
+
+        # If not a fraction turn it into a fraction #
+
+        if np.max(dataset["clt"]) > 1.0:
+            dataset["clt"] = dataset["clt"] / 100.0
+
+    return dataset
