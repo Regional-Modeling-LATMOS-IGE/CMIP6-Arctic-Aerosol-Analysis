3792cb57c997770f01151bdbc083a65726cf79cb
gibonil <188331343+gibonil@users.noreply.github.com>
2025-05-20 16:22:39 +0200

    created the framework for the extract_one_variable_per_model.py coding and testing

---
+++ utilities/tests/test_for_handle_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+Test library for extract_one_variant_per_model.py
+
+Author : GIBONI Lucas
+
+Feel free to copy, adapt and modify it under the provided license on github.
+"""
+
+##################################
+### IMPORTATION OF THE MODULES ###
+##################################
+
+import pytest   
+
---
+++ utilities/tools_for_analysis/handle_data/extract_one_variant_per_model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""
+This submodule is made to be able to extract only one variant per model
+Author : GIBONI Lucas
+
+Feel free to copy, adapt and modify it under the provided license on github.
+"""
+
+##################################
+### IMPORTATION OF THE MODULES ###
+##################################
+
+### DATA OBJECTS AND ASSOCIATED COMPUTATION ###
+
+import numpy as np  # to handle numpy arrays and the associated tools
