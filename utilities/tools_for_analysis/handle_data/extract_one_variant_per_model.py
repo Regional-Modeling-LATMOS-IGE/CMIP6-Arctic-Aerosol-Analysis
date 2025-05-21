fc9c27577906d520162a5dadc2096f988d44d503
gibonil <188331343+gibonil@users.noreply.github.com>
2025-05-21 17:01:26 +0200

    coded the function to extract source_id and tested it

--- utilities/tools_for_analysis/handle_data/extract_one_variant_per_model.py
+++ utilities/tools_for_analysis/handle_data/extract_one_variant_per_model.py
@@ -14,3 +14,42 @@ Feel free to copy, adapt and modify it under the provided license on github.
 ### DATA OBJECTS AND ASSOCIATED COMPUTATION ###
 
 import numpy as np  # to handle numpy arrays and the associated tools
+
+#########################################
+### EXTRACT THE SOURCE ID FROM A KEY ####
+#########################################
+
+def extract_source_id_from_str(key : str) -> str :
+        """
+
+        ---
+
+        ### DEFINITION ###
+
+        This function takes a key and extracts the associated source_id.
+
+        ---
+
+        ### INPUTS ###
+
+        KEY : STR | a key associated to one entry of the dictionary
+
+        ---
+
+        ### OUTPUTS ###
+
+        SOURCE_ID : STR | the extracted source_id
+
+        ---
+
+        """
+
+        ### GENERATES THE SPLITTED KEY ###
+
+        splitted_key = key.split(".")
+
+        ### FINDS THE SOURCE ID ###
+
+        source_id = splitted_key[0]
+
+        return source_id
