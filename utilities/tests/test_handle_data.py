722e20adf1868ea933c0e8083db0f0886b9628ef
gibonil <188331343+gibonil@users.noreply.github.com>
2025-05-22 12:43:54 +0200

    coded and tested the extract_one_variant_per_model package

--- utilities/tests/test_handle_data.py
+++ utilities/tests/test_handle_data.py
@@ -15,16 +15,120 @@ Feel free to copy, adapt and modify it under the provided license on github.
 ### MODULE TO BE TESTED ###
 
 from utilities.tools_for_analysis.handle_data.extract_one_variant_per_model import (
-    extract_source_id_from_str, # to extract the source id from a key
+    extract_source_id_from_str,  # to extract the source id from a key
+    get_source_id_from_keys_list,  # to generate the source_id list
+    find_first_index_for_given_source_id,  # finds the first index corresponding to a given source_id in a source_id list
+    extract_only_one_variant_keys_list,  # generates the keys' list with only one variant per source id
 )
 
+### TEST MODULE ###
+
+import pytest
+
 ############################################
 ### TESTS FOR EXTRACT_SOURCE_ID_FROM_STR ###
 ############################################
 
-def test_correct_source_id_retrieved():
+
+def test_error_if_wrong_type_extract_source_id_from_str():
+    with pytest.raises(TypeError):
+        extract_source_id_from_str(3)
+
+
+def test_correct_source_id_retrieved_extract_source_id_from_str():
     assert extract_source_id_from_str("ACCESS-CM2.r1i1p1f1.gn") == "ACCESS-CM2"
 
-############################################
-### TESTS FOR EXTRACT_SOURCE_ID_FROM_STR ###
-############################################
+
+##############################################
+### TESTS FOR GET_SOURCE_ID_FROM_KEYS_LIST ###
+##############################################
+
+
+def test_error_if_wrong_type_get_source_id_from_keys_list():
+    with pytest.raises(TypeError):
+        get_source_id_from_keys_list(3)
+
+
+def test_error_if_wrong_type_in_list_get_source_id_from_keys_list():
+    with pytest.raises(TypeError):
+        get_source_id_from_keys_list(["ACCESS-CM2", 3])
+
+
+def test_correct_source_id_list_retrieved_get_source_id_from_keys_list():
+    assert get_source_id_from_keys_list(
+        ["ACCESS-CM2.r1i1p1f1.gn", "ACCESS-ESM1-5.r1i1p1f1.gn"]
+    ) == ["ACCESS-CM2", "ACCESS-ESM1-5"]
+
+
+######################################################
+### TESTS FOR FIND_FIRST_INDEX_FOR_GIVEN_SOURCE_ID ###
+######################################################
+
+
+def test_error_if_wrong_type_given_source_id_find_first_index_for_given_source_id():
+    with pytest.raises(TypeError):
+        find_first_index_for_given_source_id(
+            given_source_id=3, source_id_list=["ACCESS-CM2", "ACCESS-ESM1-5"]
+        )
+
+
+def test_error_if_wrong_type_source_id_list_find_first_index_for_given_source_id():
+    with pytest.raises(TypeError):
+        find_first_index_for_given_source_id(
+            given_source_id="ACCESS-CM2", source_id_list=3
+        )
+
+
+def test_error_if_wrong_type_in_list_find_first_index_for_given_source_id():
+    with pytest.raises(TypeError):
+        find_first_index_for_given_source_id(
+            given_source_id="ACCESS-CM2", source_id_list=["ACCESS-CM2", 3]
+        )
+
+
+def test_correct_index_retrieved_find_first_index_for_given_source_id():
+    assert (
+        find_first_index_for_given_source_id(
+            given_source_id="ACCESS-ESM1-5",
+            source_id_list=[
+                "ACCESS-CM2",
+                "ACCESS-ESM1-5",
+                "ACCESS-ESM1-5",
+                "ACCESS-CM2",
+            ],
+        )
+        == 1
+    )
+
+
+####################################################
+### TESTS FOR EXTRACT_ONLY_ONE_VARIANT_KEYS_LIST ###
+####################################################
+
+
+def test_error_if_wrong_type_extract_only_one_variant_keys_list():
+    with pytest.raises(TypeError):
+        extract_only_one_variant_keys_list(3)
+
+
+def test_error_if_wrong_type_in_list_extract_only_one_variant_keys_list():
+    with pytest.raises(TypeError):
+        extract_only_one_variant_keys_list(["ACCESS-CM2", 3])
+
+
+def test_correct_index_retrieved_extract_only_one_variant_keys_list():
+    assert extract_only_one_variant_keys_list(
+        [
+            "IPSL-CM6A-LR.r1i1p1f1.gr",
+            "IPSL-CM6A-LR.r2i1p1f1.gr",
+            "IPSL-CM6A-LR.r3i1p1f1.gr",
+            "IPSL-CM6A-LR.r4i1p1f1.gr",
+            "IPSL-CM6A-LR-INCA.r1i1p1f1.gr",
+            "MIROC6.r11i1p1f1.gn",
+            "MIROC6.r1i1p1f1.gn",
+        ]
+    ) == [
+        "IPSL-CM6A-LR.r1i1p1f1.gr",
+        "IPSL-CM6A-LR-INCA.r1i1p1f1.gr",
+        "MIROC6.r11i1p1f1.gn",
+    ]
--- utilities/tools_for_analysis/handle_data/extract_one_variant_per_model.py
+++ utilities/tools_for_analysis/handle_data/extract_one_variant_per_model.py
@@ -19,37 +19,228 @@ import numpy as np  # to handle numpy arrays and the associated tools
 ### EXTRACT THE SOURCE ID FROM A KEY ####
 #########################################
 
-def extract_source_id_from_str(key : str) -> str :
-        """
 
-        ---
+def extract_source_id_from_str(key: str) -> str:
+    """
 
-        ### DEFINITION ###
+    ---
 
-        This function takes a key and extracts the associated source_id.
+    ### DEFINITION ###
 
-        ---
+    This function takes a key and extracts the associated source_id.
 
-        ### INPUTS ###
+    ---
 
-        KEY : STR | a key associated to one entry of the dictionary
+    ### INPUTS ###
 
-        ---
+    KEY : STR | a key associated to one entry of the dictionary
 
-        ### OUTPUTS ###
+    ---
 
-        SOURCE_ID : STR | the extracted source_id
+    ### OUTPUTS ###
 
-        ---
+    SOURCE_ID : STR | the extracted source_id
 
-        """
+    ---
 
-        ### GENERATES THE SPLITTED KEY ###
+    """
 
-        splitted_key = key.split(".")
+    ### CHECK INPUT TYPE ###
 
-        ### FINDS THE SOURCE ID ###
+    if not isinstance(key, str):
 
-        source_id = splitted_key[0]
+        raise TypeError("expected a string")
 
-        return source_id
+    ### GENERATES THE SPLITTED KEY ###
+
+    splitted_key = key.split(".")
+
+    ### FINDS THE SOURCE ID ###
+
+    source_id = splitted_key[0]
+
+    return source_id
+
+
+######################################################
+### OBTAIN THE SOURCE_ID LIST FROM THE KEYS' LIST ####
+######################################################
+
+
+def get_source_id_from_keys_list(keys_list: list[str]) -> list[str]:
+    """
+    ---
+
+    ### DEFINITION ###
+
+    This function takes the keys' lists and transforms it in a source id list.
+
+    ---
+
+    ### INPUTS ###
+
+    KEYS_LIST : LIST[STR] | the keys' list of the models' output dictionary
+
+    ---
+
+    ### OUTPUTS ###
+
+    SOURCE_ID_LIST : LIST[STR] | the extracted source_id list
+
+    ---
+
+
+    """
+
+    ### CHECK INPUT TYPE ###
+
+    ## Is the input a list ? ##
+
+    if not isinstance(keys_list, list):
+
+        raise TypeError("expected a list")
+
+    ## Are the elements of the list only str ? ##
+
+    if not all(isinstance(element, str) for element in keys_list):
+
+        raise TypeError("expected an only string list")
+
+    ### GENERATE THE SOURCE_ID LIST ###
+
+    source_id_list = [extract_source_id_from_str(key) for key in keys_list]
+
+    return source_id_list
+
+
+#######################################################################################
+### FIND THE FIRST INDEX THAT CORRESPONDS TO A GIVEN SOURCE ID IN A SOURCE_ID LIST ####
+#######################################################################################
+
+
+def find_first_index_for_given_source_id(
+    given_source_id: str, source_id_list: list[str]
+) -> int:
+    """
+
+    ---
+
+    ### DEFINITION ###
+
+    This function finds the first index that corresponds to given_source_id in a source_id list.
+
+    ---
+
+    ### INPUTS ###
+
+    GIVEN_SOURCE_ID : STR | the given source_id we look for in the list
+
+    SOURCE_ID_LIST : LIST[STR] | list of all the source_id
+
+    ---
+
+    ### OUTPUTS ###
+
+    FIRST_INDEX : INT | the first corresponding index to given_source_id
+
+    ---
+
+    """
+
+    ### CHECK INPUT TYPE ###
+
+    ## Is given_source_id a str ##
+
+    if not isinstance(given_source_id, str):
+
+        raise TypeError("expected a string")
+
+    ## Is source_id_list a list ? ##
+
+    if not isinstance(source_id_list, list):
+
+        raise TypeError("expected a list")
+
+    ## Are all the elements of the list string ? ##
+
+    if not all(isinstance(element, str) for element in source_id_list):
+
+        raise TypeError("expected an only string list")
+
+    ### FIND THE FIRST INDEX ###
+
+    ## Generates an index_list that corresponds to the index where there is a match with given_source_id ##
+
+    index_list = [
+        i for i, source_id in enumerate(source_id_list) if source_id == given_source_id
+    ]
+
+    ## Select the first index ##
+
+    first_index = index_list[0]
+
+    return first_index
+
+
+############################################################################
+### GET ONLY ONE VARIANT PER SOURCE ID FOR EVERY ENTRY IN THE KEYS_LIST ####
+############################################################################
+
+
+def extract_only_one_variant_keys_list(keys_list: list[str]) -> list[str]:
+    """
+
+    ---
+
+    ### DEFINITION ###
+
+    This function takes the keys' list of the entries' dictionary and conserves only one variant per source id.
+
+    ---
+
+    ### INPUTS ###
+
+    KEYS_LIST : LIST[STR] | the keys' list of the models' output dictionary
+
+    ---
+
+    ### OUTPUTS ###
+
+    ONLY_ONE_VARIANT_KEYS_LIST : LIST[STR] | a new keys' list for selecting only one variant per source id in the dictionary entry
+
+    ---
+
+    """
+
+    ### CHECK INPUT TYPE ###
+
+    ## Is the input a list ? ##
+
+    if not isinstance(keys_list, list):
+
+        raise TypeError("expected a list")
+
+    ### GENERATE THE SOURCE_ID LIST ###
+
+    ## Extract the source_id from every key ##
+
+    source_id_list = get_source_id_from_keys_list(keys_list)
+
+    ## Remove the duplicates ##
+
+    unique_source_id_list = np.unique(source_id_list)
+
+    ### IDENTIFY THE INDEXES TO KEEP ###
+
+    index_to_keep = [
+        find_first_index_for_given_source_id(
+            given_source_id=given_source_id, source_id_list=source_id_list
+        )
+        for given_source_id in unique_source_id_list
+    ]
+
+    ### SELECT THE ENTRIES' KEYS THAT WE PRESERVE ###
+
+    only_one_variant_keys_list = [keys_list[index] for index in index_to_keep]
+
+    return only_one_variant_keys_list
