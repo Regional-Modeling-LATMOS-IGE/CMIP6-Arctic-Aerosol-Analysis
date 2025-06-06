4f9ea0e28797843f81820a564bbbe48ff5c23c13
gibonil <188331343+gibonil@users.noreply.github.com>
2025-06-06 21:00:30 +0200

    Created the spatial average submodule and tested it.

---
+++ utilities/tests/statistical_tools/test_spatial_average.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+"""
+Test library for spatial_average.py
+
+Author : GIBONI Lucas
+
+Feel free to copy, adapt and modify it under the provided license on github.
+"""
+
+
+### MODULE TO BE TESTED ###
+
+from utilities.tools_for_analysis.statistical_tools.spatial_average import (
+    adapt_for_spatial_avgd, # to adapt the dataset attributes for regridding
+    spatial_average_given_field, # to realize the spatial average of a given field
+)
+
+
+### DATA OBJECTS AND ASSOCIATED COMPUTATION ###
+
+import xarray as xr  # to manage the data
+
+import numpy as np # to handle numpy arrays and the associated tools
+
+### TEST MODULE ###
+
+import pytest
+
+#########################################
+### TESTS FOR EADAPT_FOR_SPATIAL_AVGD ###
+#########################################
+
+### DEFINE TEST ARRAY ###
+
+test1 = xr.Dataset(
+
+    data_vars=dict(
+
+        test=(["lon", "lat"], np.array([[0]])),
+
+    ),
+
+    coords=dict(
+
+        lon=("loc", [0]),
+
+        lat=("loc", [0]),
+    ),
+
+    attrs=dict(description="Test dataset 1"),
+
+)
+
+### TESTS ###
+
+def test_attribute_latitude():
+    assert adapt_for_spatial_avgd(test1).lat.attrs["axis"] == 'Y'
+
+
+def test_attribute_longitude():
+    assert adapt_for_spatial_avgd(test1).lon.attrs["axis"] == 'X'
+
+
+#############################################
+### TESTS FOR SPATIAL_AVERAGE_GIVEN_FIELD ###
+#############################################
+
+test_map =  np.array([[2, 1, 2], [1, 0, 1], [2, 1, 2]])
+                     
+arr = xr.DataArray(
+
+    data= test_map,
+
+    dims=["lon", "lat"],
+
+    coords={"lon": range(3), "lat": range(3)},
+
+    attrs=dict(description="Test dataset 2"),
+
+)
+
+test2 = xr.Dataset({"test": arr})
+
+test2 = adapt_for_spatial_avgd(test2)
+
+def test_good_result_average():
+    assert spatial_average_given_field(field = "test", dataset = test2) == np.round(np.mean(test_map),2)
--- utilities/tools_for_analysis/statistical_tools/spatial_average.py
+++ utilities/tools_for_analysis/statistical_tools/spatial_average.py
@@ -6,3 +6,107 @@ Author : GIBONI Lucas
 
 Feel free to copy, adapt and modify it under the provided license on github.
 """
+
+###################################
+### IMPORTATIONS OF THE MODULES ###
+###################################
+
+### DATA OBJECTS AND ASSOCIATED COMPUTATION ###
+
+import numpy as np  # to handle numpy arrays and the associated tools
+
+import xarray as xr  # to manage the data
+
+import xcdat as xc  # to handle climate model outputs with xarray
+
+### TYPE HINTS FOR FUNCTIONS ###
+
+from numpy.typing import NDArray  # type hints for numpy
+
+###############################################################
+### ADAPT THE ARRAYS FOR DOING A SPATIAL AVERAGE WITH XCDAT ###
+###############################################################
+
+def adapt_for_spatial_avgd(dataset : xr.Dataset) -> xr.Dataset :
+
+    """
+
+    ---
+
+    ### DEFINITION ###
+
+    This function changes the attributes of the axes in a dataset. This is mandatory to use the spatial averaging techniques of xcdat.
+
+    ---
+
+    ### INPUTS ###
+
+    DATASET : XR DATASET | dataset that will get its attributes changed
+
+    ---
+
+    ### OUTPUTS ###
+
+    DATASET : XR.DATASET | dataset with its attributes changed
+
+    ---
+    """
+
+    ### CHANGE THE ATTRIBUTES OF THE AXES OF DATASET ###
+
+    ## LATITUDE ##
+
+    dataset.lat.attrs["axis"] = "Y"
+
+    ## LONGITUDE ##
+
+    dataset.lon.attrs["axis"] = "X"
+
+    ## SET BOUNDS ##
+
+    dataset = dataset.bounds.add_missing_bounds()
+
+    return dataset
+
+########################################
+### SPATIAL AVERAGE OF A GIVEN FIELD ###
+########################################
+
+def spatial_average_given_field(field : str, dataset : xr.Dataset) -> NDArray[np.float64]:
+
+    """
+
+    ---
+
+    ### DEFINITION ###
+
+    This function changes computes the spatial average of a provided input field on a given dataset.
+
+    ---
+
+    ### INPUTS ###
+
+    FIELD : STR | field to be averaged 
+
+    DATASET : XR DATASET | dataset holding the variable to average
+
+    ---
+
+    ### OUTPUTS ###
+
+    SPATIAL_AVG : NUMPY ARRAY OF FLOAT 64 | the spatial average of the dataset fields.Its dimensions depends of the time dimension of the dataset.
+
+    ---
+    """
+
+    ### COMPUTE THE SPATIAL AVERAGE ###
+
+    ## Generate the spatial average ##
+
+    spatial_avg = dataset.spatial.average(field, axis=["X", "Y"])[field].values
+
+    ## Round it to 2 digits after the comma ##
+
+    spatial_avg = np.round(spatial_avg, 2)
+
+    return spatial_avg
